86608e14d91ce854d8631a7832500e72225e7ffd
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-10-02 15:51:57 -0400

    2

--- p1.js
+++ p1.js
@@ -1,8 +1,9 @@
 var rounds = prompt("How many rounds");
 var compChoice = math.floor(Math.random() * 3);
-var r = parseInt(rounds) + "<br>"
-for(let i = 0; <= rounds; i++)
+var r = parseInt(rounds)
+for(let i = 0; <= r; i++)
 var player = prompt("Pick one R / P / S ");
+
 
 if(compChoice = 0){
     compChoice = "rock";
