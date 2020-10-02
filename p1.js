700329aa4eb5e513cd4caf75ae53432bc4d2fdf5
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-10-02 14:54:35 -0400

    initial commit

---
+++ p1.js
@@ -0,0 +1,15 @@
+var rounds = prompt("How many rounds");
+var compChoice = math.floor(Math.random() * 3);
+var r = parseInt(rounds) + "<br>"
+for(let i = 0; <= rounds; i++)
+var player = prompt("Pick one R / P / S ");
+
+if(compChoice = 0){
+    compChoice = "rock";
+} else if(1 <= compChoice <= 2){
+    compChoice = "paper";
+} else{
+    compChoice = "scissors";
+}
+
+       
