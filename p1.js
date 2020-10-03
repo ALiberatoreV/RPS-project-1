c2ecc0b60f5e98685a57bff9938bc2d7c8c154de
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-10-02 20:01:49 -0400

    A commit 4

--- p1.js
+++ p1.js
@@ -2,15 +2,51 @@ var rounds = prompt("How many rounds");
 var compChoice = math.floor(Math.random() * 3);
 var r = parseInt(rounds)
 for(let i = 0; <= r; i++)
-var player = prompt("Pick one R / P / S ");
+var player = prompt("Pick one rock / paper / sicssors ");
 
 
 if(compChoice = 0){
     compChoice = "rock";
-} else if(1 <= compChoice <= 2){
+} else if(compChoice === 1){
     compChoice = "paper";
 } else{
     compChoice = "scissors";
 }
+choice1 = player;
+choice2 = compChoice;
 
-       
+function compare( choice1, choice2){
+    if(choice1 === choice2){
+        alert("Tie");
+    }
+    if(choice1 === "paper"){
+        if(choice2 === "rock"){
+             alert("player wins");
+            
+        }
+    }
+    if(choice1 === "paper"){
+        if(choice2 === "scissors"){
+            alert("player loses");
+        }
+    }
+}   if(choice1 === "rock"){
+        if(choice2 === "scissors"){
+            alert("player wins")
+        }
+    if(choice1 === "rock"){
+        if(choice2 === "paper"){
+            alert("player loses")
+        }
+    }
+    if(choice1 === "scissors"){
+        if(choice2 === "paper"){
+            alert("player wins")
+        }
+    }
+    if(choice1 === "scissors"){
+        if(choice2 === "rock"){
+            alert("player loses")
+        }
+    }
+} 
