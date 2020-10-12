e3a85f80c4449bc81c4fe38bf7c4c00e1626703f
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-10-12 16:12:25 -0400

    AFC

--- p1.js
+++ p1.js
@@ -1,53 +1,60 @@
 var rounds = prompt("How many rounds");
-var compChoice = Math.floor(Math.random() * 3);  //something is not working here
-var r = parseInt(rounds);
-for(let i = 0;  r; i++);
-var player = prompt("Pick one rock / paper / sicssors");
 
+var r = parseInt(rounds);
+// for(let i = 0;  r; i++);
+
+var tie = 0;
+var wins = 0;
+var losses = 0;
+
+for(let i = 0; i < r; i++){
+    var player = prompt("Pick one rock / paper / scissors");
+    var compChoice = Math.floor(Math.random() * 3);  //something is not working here
 
 if(compChoice === 0){
-    compChoice === "rock";
+    compChoice = "rock";
+    
+    if(player === compChoice){
+        tie += 1;
+        alert("you tied " + tie + " times");
+    }else if(player === "paper"){
+        wins += 1;
+        alert("you Win " + wins + " times");
+    }else if(player === "scissors"){
+        losses += 1;
+        alert("You lose " + losses + " times");
+    }
 } else if(compChoice === 1){
-    compChoice === "paper";
+    compChoice = "paper";
+    
+    if(player === compChoice){
+        tie += 1;
+        alert("you tied " + tie + " times");
+    }else if(player === "scissors"){
+        wins += 1;
+        alert("you Win " + wins + " times");
+    }else if(player === "rock"){
+        losses += 1;
+        alert("You lose " + losses + " times");
+    }
 } else{
-    compChoice === "scissors";
-}
+    compChoice = "scissors";
+    
+    if(player === compChoice){
+        tie += 1;
+        alert("you tied " + tie + " times");
+    }else if(player === "rock"){
+        wins += 1;
+        alert("you Win " + wins + " times");
+    }else if(player === "paper"){
+        losses += 1;
+        alert("You lose " + losses + " times");
+    }
+}}
+alert("wins " + wins + " losses " + losses + " ties " + tie );
 choice1 = player;
 choice2 = compChoice;
 
-function compare( choice1, choice2){
-    if(choice1 === choice2){
-        alert("Tie");
-    }
-    if(choice1 === "paper"){
-        if(choice2 === "rock"){
-             alert("player wins");
-            
-        }
-    }
-    if(choice1 === "paper"){
-        if(choice2 === "scissors"){
-            alert("player loses");
-        }
-    }
-}   if(choice1 === "rock"){
-        if(choice2 === "scissors"){
-            alert("player wins")
-        }
-    if(choice1 === "rock"){
-        if(choice2 === "paper"){
-            alert("player loses");
-        }
-    }
-    if(choice1 === "scissors"){
-        if(choice2 === "paper"){
-            alert("player wins");
-        }
-    }
-    if(choice1 === "scissors"){
-         if(choice2 === "rock"){
-            alert("player loses");
-        }
-    }
-}   
+
+
 console.log(compChoice)
